29baa64c033231a97d5d13dd83226988b9ba1a99
보경 <wbk0754@naver.com>
2024-05-22 15:03:03 +0900

    2024.05.22

--- js/test.js
+++ js/test.js
@@ -16,3 +16,10 @@ if (true) {
     //e = 5;
     //const f // Error?
     console.log(e);
+
+const search_message = () => {
+    const c = '검색을 수행합니다';
+    alert(c);
+    };
+    
+    document.getElementById("search_btn").addEventListener('click', search_message);
